1c73bd03b44fd73cd5c8a50e9623d7e30554f72d
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-09-14 08:26:32 -0700

    heroku config

--- knexfile.js
+++ knexfile.js
@@ -9,15 +9,15 @@ module.exports = {
 
 
   production: {
-    client: 'postgresql',
-    connection:  process.env.DATABASE_URL || 'postgres://localhost/youniversity',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  client: 'postgresql',
+  connection: process.env.DATABASE_URL,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
   }
+}
 
 };
