57b951bb0d84361f26e4488b26eb770b849a18ba
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-09-23 10:51:08 -0700

    query to grab thread messages works

--- routes/messages.js
+++ routes/messages.js
@@ -12,12 +12,15 @@ router.get('/:id/', function(req, res, next) {
     .join('users AS sender', {'message_threads.sender_id' : 'sender.id'})
     .join('users AS recipient', {'message_threads.recipient_id' : 'recipient.id'})
     .join('classes', {'message_threads.class_id' : 'classes.id'})
+    // .join('messages', {'messages.thread_id' : 'message_threads.id'})
 
-    .select("sender.id AS sender_id", "sender.first_name AS sender_first_name", "sender.last_name AS sender_last_name", "recipient.id AS recipient_id", 'recipient.first_name AS recipient_first_name',  'recipient.last_name AS recipient_last_name', 'title', 'creation_date')
+    .select("message_threads.id AS id", "sender.id AS sender_id", "sender.first_name AS sender_first_name", "sender.last_name AS sender_last_name", "recipient.id AS recipient_id", 'recipient.first_name AS recipient_first_name',  'recipient.last_name AS recipient_last_name', 'title', 'unread_messages')
 
     .where({'message_threads.sender_id' : req.params.id})
     .orWhere({'message_threads.recipient_id' : req.params.id})
-    
+
+    // .orderBy('creation_date')
+
     .then(function(data){
       res.send(data);
     })
@@ -27,17 +30,19 @@ router.get('/:id/', function(req, res, next) {
 });
 
 
-//get all of one user's messages
+//get all of one thread's messages
 router.get('/thread/:id', function(req, res, next) {
   return knex('message_threads')
     .join('messages', {'message_threads.id' : 'messages.thread_id'})
-    .join('users', {'message_threads.sender_id' : 'users.id'})
+    // .join('users', {'message_threads.sender_id' : 'users.id'})
     // .join('users', {'message_threads.recipient_id' : 'users.id'})
     // .select('*', 'messages.id AS id', 'message_threads.id AS thread_id')
-    .where({'message_threads.sender_id' : req.params.id})
-    .orWhere({'message_threads.recipient_id' : req.params.id})
-    .select('*')
-    // .orWhere({'recipients_id' : req.params.user_id})
+    .where({'message_threads.id' : req.params.id})
+
+    .select('message', 'creation_date', 'read')
+
+    .orderBy('creation_date')
+
     .then(function(data){
       res.send(data);
     })
